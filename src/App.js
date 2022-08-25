80eda9c5655074c6fb8f6621e1fcac6920f15e95
Daryle Karl De Vera <darylekarl.devera@gmail.com>
2022-08-25 19:20:47 +0800

    delete state hook

--- src/App.js
+++ src/App.js
@@ -6,30 +6,6 @@ import EditLayout from './component/Function/EditLayout';
 import FullPage from './component/Content/FullPage'
 import{Routes, Route} from 'react-router-dom'
 function App() {
-  const [itemData, setItemData] = React.useState([
-    {
-      id: 1,
-      imageURL : 'https://via.placeholder.com/300x250?',
-      name: 'Item 1',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam voluptatum beatae aliquid consectetur suscipit, sint sapiente dolores, unde officia omnis quae placeat laboriosam distinctio maxime itaque at harum voluptas?',
-      price: '500'
-    },
-    {
-      id: 2,
-      imageURL : 'https://via.placeholder.com/300x250?',
-      name: 'Item 2',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam voluptatum beatae aliquid consectetur suscipit, sint sapiente dolores, unde officia omnis quae placeat laboriosam distinctio maxime itaque at harum voluptas?',
-      price: '500'
-    },
-    {
-      id: 3,
-      imageURL : 'https://via.placeholder.com/300x250?',
-      name: 'Item 3',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam voluptatum beatae aliquid consectetur suscipit, sint sapiente dolores, unde officia omnis quae placeat laboriosam distinctio maxime itaque at harum voluptas?',
-      price: '500'
-    }
-  ]);
-  // console.log(storeItem.name)
   return (
     <div className="App">
       <Routes>
