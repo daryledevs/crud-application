8c5a2114bde726fd1bdd5762cdf63aed7effe6bc
Daryle Karl De Vera <darylekarl.devera@gmail.com>
2022-08-31 04:56:32 +0800

    put back header

--- src/component/BaseLayout.jsx
+++ src/component/BaseLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './base/Header';
 import Navigation from './base/Navigation';
 import Footer from './base/Footer';
 
@@ -7,9 +8,7 @@ import {Outlet} from 'react-router-dom';
 const BaseLayout = () => {
   return (
     <div className='base-layout-container'>
-      <header className='header-container'>
-        <h1>CRUD Application</h1>
-      </header>
+      <Header/>      
       <Navigation/>
       <Outlet/>
       <Footer/>
